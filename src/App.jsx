3605ffddba1c10c710f801704739f49312fe9a3b
Matos52 <Matej.Regec@gmail.com>
2024-05-17 23:32:10 +0200

    Added validation to Signup form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import React from 'react';
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
 import Signup from './components/SignUp.jsx';
-import StateLogin from './components/StateLogin.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <StateLogin />
+        <Signup />
       </main>
     </>
   );
